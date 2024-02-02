ac914939cfaa242d037201672d3db94c2c2211ee
daga96 <dagaluk96@gmail.com>
2024-02-02 11:53:29 +0900

    feat: grid creation

--- client/src/client.js
+++ client/src/client.js
@@ -30,15 +30,45 @@ const onChatSubmitted = (sock) => (e) => {
   sock.emit("message", text);
 };
 
-const createBoard = (canvas) => {
+const createBoard = (canvas, numCells = 20) => {
   const ctx = canvas.getContext("2d");
+  const cellSize = Math.floor(canvas.width / numCells);
 
-  const fillRect = (x, y, color) => {
+  const fillCell = (x, y, color) => {
     ctx.fillStyle = color;
-    ctx.fillRect(x - 10, y - 10, 20, 20);
+    ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
   };
 
-  return { fillRect };
+  const drawGrid = () => {
+    ctx.strokeStyle = "#333";
+    ctx.beginPath();
+
+    for (let i = 0; i < numCells + 1; i++) {
+      ctx.moveTo(i * cellSize, 0);
+      ctx.lineTo(i * cellSize, cellSize * numCells);
+      ctx.moveTo(0, i * cellSize);
+      ctx.lineTo(cellSize * numCells, i * cellSize);
+    }
+    ctx.stroke();
+  };
+
+  const clear = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  };
+
+  const reset = () => {
+    clear();
+    drawGrid();
+  };
+
+  const getCellCoordinates = (x, y) => {
+    return {
+      x: Math.floor(x / cellSize),
+      y: Math.floor(y / cellSize),
+    };
+  };
+
+  return { fillCell, reset, getCellCoordinates };
 };
 
 const getCoordinates = (el, e) => {
@@ -51,20 +81,22 @@ const getCoordinates = (el, e) => {
 (function () {
   const canvas = document.querySelector("canvas");
 
-  const { fillRect } = createBoard(canvas);
+  const { fillCell, reset, getCellCoordinates } = createBoard(canvas);
   const sock = io();
 
   const onClick = (e) => {
     const { x, y } = getCoordinates(canvas, e);
-    sock.emit("turn", { x, y });
+    sock.emit("turn", getCellCoordinates(x, y));
   };
+
+  reset();
 
   sock.on("message", (text) => {
     show(text);
   });
 
-  sock.on("turn", ({ x, y }) => {
-    fillRect(x, y);
+  sock.on("turn", ({ x, y, color }) => {
+    fillCell(x, y, color);
   });
 
   document
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const randomColor = require("randomcolor");
 const app = express();
 
 app.use(express.static(`${__dirname}/../client`));
@@ -9,11 +10,13 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (sock) => {
+  const color = randomColor();
+
   sock.emit("message", "You are connected");
   sock.on("message", (text) => io.emit("message", text));
 
   sock.on("turn", ({ x, y }) => {
-    io.emit("turn", { x, y });
+    io.emit("turn", { x, y, color });
   });
 });
 
